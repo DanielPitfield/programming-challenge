22dfbfffcf75279cbf8df5569b8a18944666cdfe
Daniel Pitfield <danielpitfield1@gmail.com>
2023-02-01 17:39:38 +0000

    decouple punctuation whitelist (within words)

--- utils/getTrimmedWord.ts
+++ utils/getTrimmedWord.ts
@@ -1,7 +1,9 @@
 // The words of the text are split by a delimiter of " " and therefore sometimes end with punctuation
 export function getTrimmedWord(word: string): string {
-  // Pattern of anything that's not a digit, letter, whitespace (alphanumeric)
-  const regex = /[^\w\s\'-]/g;
+  // What punctuation WITHIN words should be matched (and therefore kept)?
+  const punctuationExceptions: string[] = ["-", "_"];
+  // Pattern of anything that's not a digit, letter, whitespace (or included in the above exceptions)
+  const regex = new RegExp(`[^\\w\\s\\${punctuationExceptions.join("")}]`, "g");
 
   return word.replaceAll(regex, "");
 }
