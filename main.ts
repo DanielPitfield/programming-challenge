4c3214ee59c4ee0baa818b22a3e787ab81669ba4
Daniel Pitfield <danielpitfield1@gmail.com>
2023-02-01 15:08:58 +0000

    Log outputs

---
+++ main.ts
@@ -0,0 +1,21 @@
+import { findTermInstances } from "./utils/findTermInstances";
+
+// There are also test suites with these examples, but still log them to more easily see the output!
+
+// ["Customer"]
+console.log(findTermInstances("The Customer is always right", "Customer, you"));
+
+// ["Customer", "our"]
+console.log(findTermInstances("The Customer is not our client", "Customer, us"));
+
+// ["My", "myself", "Client"]
+console.log(findTermInstances("My rights cannot be abridged by myself, only the Client", "I, Client"));
+
+// ["my"]
+console.log(findTermInstances("i) In this clause my documents are read", "Me"));
+
+// []
+console.log(findTermInstances("The customer is always right", "Customer"));
+
+// ["Customer", "Customer"]
+console.log(findTermInstances("The Customer is always the Customer", "Customer"));
