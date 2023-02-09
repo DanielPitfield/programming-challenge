4b30edcd20730358aa4e8dd2ee0581e656c8bb8d
Daniel Pitfield <danielpitfield1@gmail.com>
2023-02-09 18:23:26 +0000

    sets (optimisation)

--- utils/findTermInstances.ts
+++ utils/findTermInstances.ts
@@ -6,9 +6,13 @@ import { getTrimmedWord } from "./getTrimmedWord";
 export function findTermInstances(text: string, terms: string): string[] {
   // The words of the text string
   const textWords: string[] = text.split(" ").map((word) => getTrimmedWord(word));
-  const allTerms: string[] = terms.split(", ");
 
-  const mappedTerms: string[] = allTerms
+  // The terms to match/look for in the text
+  const allTerms: string[] = terms.split(", ");
+  // Remove any duplicate or repeated terms
+  const uniqueTerms: Set<string> = new Set(allTerms);
+
+  const mappedTerms: string[] = Array.from(uniqueTerms)
     .map((term) => {
       if (isPronoun(term)) {
         // Any matching pronouns which are within the text
@@ -17,7 +21,7 @@ export function findTermInstances(text: string, terms: string): string[] {
 
       // Not a pronoun but the text has this term
       if (textWords.some((word) => word === term)) {
-        return [term];
+        return term;
       }
 
       // Term is not within text
@@ -25,5 +29,8 @@ export function findTermInstances(text: string, terms: string): string[] {
     })
     .flat();
 
-  return textWords.filter((textWord) => mappedTerms.some((term) => term === textWord));
+  // Different pronouns may have been mapped to the same pronoun group
+  const uniqueMappedTerms: Set<string> = new Set(mappedTerms);
+
+  return textWords.filter((textWord) => Array.from(uniqueMappedTerms).some((term) => term === textWord));
 }
